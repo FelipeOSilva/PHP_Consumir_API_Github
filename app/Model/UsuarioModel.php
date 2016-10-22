d7e1e1e7f297c682746184aacc0a135dfe9f50fd
Felipe Silva <felipe_96sp@msn.com>
2016-10-22 02:29:14 -0200

    Adição de novos atributos ao UsuarioModel

--- app/Model/UsuarioModel.php
+++ app/Model/UsuarioModel.php
@@ -4,11 +4,15 @@ do  retorno da pesquisa, realizando os gets e sets*/
 class UsuarioModel {
 	private $usuario;
 	private $avatar;
+  private $repositorio;
+  private $urlUsuario;
 
   /*Construtor para receber um json e setar os valores nas devidas variaveis*/
   public function __construct($json){
 		$this->setUsuario($json->login);
 		$this->setAvatarUrl($json->avatar_url);
+    $this->setRepositorio($json->repos_url);
+		$this->setUrlUsuario($json->html_url);
 	}
 
 	public function setUsuario($usuario){
@@ -22,5 +26,17 @@ class UsuarioModel {
 	}
 	public function getAvatarUrl(){
 		return $this->avatar;
+	}
+  public function setRepositorio($repositorio){
+		$this->repositorio = $repositorio;
+	}
+	public function getRepositorio(){
+		return $this->repositorio;
+	}
+	public function setUrlUsuario($urlUsuario){
+		$this->urlUsuario = $urlUsuario;
+	}
+	public function getUrlUsuario(){
+		return $this->urlUsuario;
 	}
 }
