6b9155812445949113567857c9e77e6205302a1c
Felipe Silva <felipe_96sp@msn.com>
2016-10-22 01:20:53 -0200

    Criação do campo de pesquisa de busca do usuario

---
+++ app/View/pesquisar_usuarios.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <h3>Busca de Usuarios do Github</h3>
+  <form method="get">
+    <div class="form-group">
+      <input class="form-group form-control" type="text" name="usuario" placeholder="Digite o nome do usuário" value="">
+      <button class="btn btn-primary" type="submit">Buscar</button>
+    </div>
+  </form>
+</div>
